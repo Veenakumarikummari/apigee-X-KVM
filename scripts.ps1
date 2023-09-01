46f95447efa6e006f9f8b82983e3867edbc87ae7
Veenakumarikummari <142590452+Veenakumarikummari@users.noreply.github.com>
2023-09-01 22:47:52 +0530

    Update scripts.ps1

--- scripts.ps1
+++ scripts.ps1
@@ -1,35 +1,110 @@
 # write-output Apigee Artifacts
 $token = $env:TOKEN
 $org = $env:ORG
-$baseURL = "https://apigee.googleapis.com/v1/organizations/"
-$headers = @{Authorization = "Bearer $token"}
+# $baseURL = "https://apigee.googleapis.com/v1/organizations/"
+# $headers = @{Authorization = "Bearer $token"}
 
 # Set your GitHub repository information
-$repositoryOwner = "veenakumari.kummari@easystepin.com"
-$repositoryName = "apigee-X-KVM"
+$repositoryOwner = "rajeshjanapati@gmail.com"
+$repositoryName = "github-kvm-apigeex"
 $branchName = "main"
 
 # Clone the repository
-git clone https://github.com/Veenakumarikummari/apigee-X-KVM.git
-cd apigee-X-KVM
-
- 
+git clone https://github.com/rajeshjanapati/github-kvm-apigeex.git
+cd $repositoryName
+cd kvms
 
 # Read JSON files
-$filePath = apiconfig/env/eval/kvms.json -Recurse
-$jsonContent = Get-Content $filePath -Raw
-$apiUrl = "https://apigee.googleapis.com/v1/organizations/esi-apigee-x-394004/environments/eval/keyvaluemaps"
-$headers = @{
-	"Authorization" = "Bearer $token"
-	"Content-Type" = "application/json"
+$jsonFiles = Get-ChildItem -Filter *.json -Recurse
+
+# Loop through each JSON file and make POST requests
+foreach ($jsonFile in $jsonFiles) {
+    Write-Host "Entered into foreach..."
+    $jsonContent = Get-Content -Path $jsonFile -Raw
+    # Parse the JSON content
+    $jsonData = ConvertFrom-Json $jsonContent
+
+    # Extract the value of the "name" key from the JSON data
+    $kvmName = $jsonData.name
+
+    # Print the extracted value
+    Write-Host "KVM Name: $kvmName"
+
+    $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+    $headers.Add("Authorization", "Bearer ya29.a0AfB_byBVRRWv3j120nTXVXah3wvqwIpBIGNguhajsUiE87R496vX-zJEJrL3ZLCcGyBkxL5Ynuu61HEwz9nn6_evRSewYFWQT3gi4mrocvzdmo8odRtPVYeFMgXVk7lerSLLom8dDkKCWJCRcNq7yKKM_kIgl_JUZv_USetE1bmNWUwaCgYKAZQSARESFQHsvYlsDh3J5BDbCEj488voROYSPA0182")
+    $headers.Add("Content-Type", "application/json")
+
+    $body = @"
+    {
+        "name": $kvmName,
+        "encrypted": true  # Set to false if you don't want encryption
+    }
+"@
+
+    $response = Invoke-RestMethod 'https://apigee.googleapis.com/v1/organizations/esi-apigee-x-394004/environments/eval/keyvaluemaps' -Method 'POST' -Headers $headers -Body $body
+    $response | ConvertTo-Json
+    # Check the HTTP status code and handle errors
+    if ($response.StatusCode -eq 200) {
+        Write-Host "KVM created successfully."
+    } else {
+        Write-Host "Error creating KVM. Status Code: $($response.StatusCode)"
+        Write-Host "Response Content: $($response.Content)"
+    }
 }
 
+  
+
+  
+    
+    # $jsonData = ConvertFrom-Json $jsonContent
+    # Write-Host $jsonData
+    # foreach ($name in $($jsonData)) {
+    #     Write-Host $name
+    #     $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+    #     $headers.Add("Authorization", "Bearer ya29.a0AfB_byDVwfNZVNiTBsatmu7gJEQyPExZ3TBhYONcediO5NcqjC6jf1o34DmhvKWHc999CUnVFJfjfjkELG3OFRGebsOAPMvoJmLsRccgc4gbDtwqWfVbrI_1STm9yQhpoxFpnPKLZQY5K0YCu9U0sNZaeRnz31PTu-vWYR5Px7HKYXkaCgYKAaISARESFQHsvYls6LkmPwzCEYsHSFPkVZQVDA0182")
+    #     $headers.Add("Content-Type", "application/json")
+        
+    #     $body = @"
+    #     {
+    #         `"name`":`"test-pst11`",
+    #         `"encrypted`":`"true`"
+    #     }
+    #     "@
+        
+    #     $response = Invoke-RestMethod 'https://apigee.googleapis.com/v1/organizations/esi-apigee-x-394004/environments/eval/keyvaluemaps' -Method 'POST' -Headers $headers -Body $body
+    #     $response | ConvertTo-Json
+    # }
 
 
-response = Invoke-RestMethod -Uri $apiUrl -Method Post -Headers $headers -Body $jsonContent
+#     $kvmData = $jsonContent | ConvertFrom-Json
+#     Write-Host $kvmData
+#     foreach ($entry in $kvmData) {
+#         $key = $entry.Key
+#         $value = $entry.Value
+    
+#         $kvmEntry = @{
+#             "name" = $key
+#             "encrypted" = "false"
+#             "entry" = $value
+#         } | ConvertTo-Json
+#     $baseUrl = "https://api.enterprise.apigee.com/v1/organizations/esi-apigee-x-394004/environments/eval/keyvaluemaps"
+
+#     $kvmUrl = $baseUrl+$key+"/entries"
+#     Invoke-RestMethod -Uri $kvmUrl -Method Post -Headers $headers -Body $kvmEntry
+# }
 
 
+# $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+# $headers.Add("Authorization", "Bearer ya29.a0AfB_byDVwfNZVNiTBsatmu7gJEQyPExZ3TBhYONcediO5NcqjC6jf1o34DmhvKWHc999CUnVFJfjfjkELG3OFRGebsOAPMvoJmLsRccgc4gbDtwqWfVbrI_1STm9yQhpoxFpnPKLZQY5K0YCu9U0sNZaeRnz31PTu-vWYR5Px7HKYXkaCgYKAaISARESFQHsvYls6LkmPwzCEYsHSFPkVZQVDA0182")
+# $headers.Add("Content-Type", "application/json")
 
-Write-Host "uploded"
+# $body = @"
+# {
+#     `"name`":`"test`",
+#     `"value`":`"12345`"
+# }
+# "@
 
+# $response = Invoke-RestMethod 'https://apigee.googleapis.com/v1/organizations/esi-apigee-x-394004/environments/eval/keyvaluemaps/github-KVM/entries' -Method 'POST' -Headers $headers -Body $body
+# $response | ConvertTo-Json
 
