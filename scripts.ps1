b07faeeb9242b691208b1c1d642168641c89d462
Veenakumarikummari <142590452+Veenakumarikummari@users.noreply.github.com>
2023-09-01 14:58:35 +0530

    Update scripts.ps1

--- scripts.ps1
+++ scripts.ps1
@@ -16,8 +16,8 @@ cd apigee-X-KVM
  
 
 # Read JSON files
-$jsonFiles = apiconfig/env/eval/kvms.json -Recurse
-$jsonContent = Get-Content $jsonFiles -Raw
+$filePath = = apiconfig/env/eval/kvms.json -Recurse
+$jsonContent = Get-Content -Path $filePath -Raw
 $apiUrl = "https://apigee.googleapis.com/v1/organizations/esi-apigee-x-394004/environments/eval/keyvaluemaps"
 $headers = @{
 	"Authorization" = "Bearer $token"
